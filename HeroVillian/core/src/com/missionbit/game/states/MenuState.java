0ad394e19eee406d5aec5325ffd58db4e0030749
Cintia-02 <arambulacintia12@gmail.com>
2017-10-10 16:53:01 -0700

    added states

---
+++ HeroVillian/core/src/com/missionbit/game/states/MenuState.java
@@ -0,0 +1,43 @@
+package com.missionbit.game.states;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.missionbit.game.HeroVillian;
+
+/**
+ * Created by missionbit on 10/5/17.
+ */
+
+public class MenuState extends State{
+
+//    private Texture background;
+//    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+
+//        background = new Texture("bg.png");
+//        playBtn = new Texture("playBtn.png");
+//        cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
